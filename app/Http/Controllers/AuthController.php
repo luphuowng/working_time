f3a21f15bb854fbdcef6b9a466ab7334ff8603e3
DESKTOP-QQKCOR3\luphu <luphuowng@gmail.com>
2021-08-04 00:24:25 +0700

    update api register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -47,6 +47,7 @@ class AuthController extends Controller
     public function register(Request $request){
 
         $validator = Validator::make($request->all(),[
+            'username' => 'required|string|between:5,100',
             'name' => 'required|string|between:2,100',
             'email' => 'required|string|email|max:100|unique:users',
             'password' => 'required|string|confirmed|min:6',
