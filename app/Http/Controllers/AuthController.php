7dd1148c1d1cc6bed249c9a69ab8456e1383d5cd
DESKTOP-QQKCOR3\luphu <luphuowng@gmail.com>
2021-08-04 23:43:53 +0700

    change register

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -47,7 +47,6 @@ class AuthController extends Controller
     public function register(Request $request){
 
         $validator = Validator::make($request->all(),[
-            'username' => 'required|string|between:5,100',
             'name' => 'required|string|between:2,100',
             'email' => 'required|string|email|max:100|unique:users',
             'password' => 'required|string|confirmed|min:6',
