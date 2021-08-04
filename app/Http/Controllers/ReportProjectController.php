08f6e18024d6a0bd5bb72900e56aee06ab49ce88
DESKTOP-QQKCOR3\luphu <luphuowng@gmail.com>
2021-08-05 02:47:50 +0700

    fix m

--- app/Http/Controllers/ReportProjectController.php
+++ app/Http/Controllers/ReportProjectController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\models\ReportProject;
+use App\Models\ReportProject;
 use App\Models\workingtime;
 use App\Models\User;
 use App\Models\Project;
